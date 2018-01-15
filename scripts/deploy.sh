5af784074493faf8191664e2c9cd3e3a981926f1
Aikdanai Sidhikosol <aikdanai.s@gmail.com>
2018-01-16 03:11:13 +0700

    deployment install nvm and npm on its own

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd ~/homepage-aikdanai
 git pull
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+nvm install 6.8.1
 npm install -g yarn
 yarn
 yarn build
