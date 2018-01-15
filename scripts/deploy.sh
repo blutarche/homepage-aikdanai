62640e5df20c268e63b0d434cbaa5537da210463
Aikdanai Sidhikosol <aikdanai.s@gmail.com>
2018-01-16 03:31:59 +0700

    cleanup deploy shell script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd ~/homepage-aikdanai
 git pull
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-nvm install 6.8.1
-npm install -g yarn
 yarn
 yarn build
 cp -R ./build/* /var/www/html/
