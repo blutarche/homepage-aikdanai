8c46f9fac402c8d58e31a0d84da5c0bbaa40b06c
Aikdanai Sidhikosol <aikdanai.s@gmail.com>
2018-02-04 00:03:01 +0700

    update info section

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
 import './Info.css';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class Info extends Component {
   render() {
     return (
       <div className="Info">
-        <div className="Info-picture">
-          [Dear future me, insert my picture here.]
-        </div>
-        <div className="Info-content">
-          <p>
-            <span className="Info-content-title">Hey there!</span>
-          </p>
-          <p>
-            Nice to meet you. I am <a href="https://facebook.com/aikdanai.s" rel="noopener noreferrer" target="_blank" className="color-primary">Aikdanai Sidhikosol</a>, a full-stack web developer and a game developer.
-          </p>
-          <p>
-            For website, I mostly developed on front-end side of things with a little bit of back-end here and there. But I am interested in both. I love to try new technologies or libraries to complete different requirements each project has, in which case web development satisfied that needs quite well.
-          </p>
-          <p>
-            As for game development, even though I never have been in the game company or developed any large-scale games (or even any middle-scale one), I am still passionate about it. I have developed some small games as a hobby and it has been a lot of fun!
-          </p>
-        </div>
+        <Grid>
+          <Row>
+            <Col sm={6}>
+              <div className="Info-picture">
+                [Dear future me, insert my picture here.]
+              </div>
+            </Col>
+            <Col sm={6}>
+              <div className="Info-content">
+                <p>
+                  <span className="Info-content-title">Hey there!</span>
+                </p>
+                <p>
+                  I am <a href="https://facebook.com/aikdanai.s" rel="noopener noreferrer" target="_blank" className="color-primary">Aikdanai Sidhikosol</a>, a full-stack web developer and a game developer.
+                </p>
+                <p>
+                  For website, I focused on front-end side of things and doing some back-end at times. Although I am interested in both. Web technologies was and still is changing fast so us developers have to adapt, but trying out new things is interesting so it's all good!
+                </p>
+                <p>
+                  As for game development, even though I never have been in the game company or developed any large-scale games (or even any middle-scale one), I am still passionate about it. I have developed some small games as a hobby and it has been a lot of fun!
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
     	</div>
     );
   }
